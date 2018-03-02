c47970ce706cf2a592c301cc26f99c643de393f9
[raimonomiar] <[raimonomiar@gmail.com]>
2018-03-02 21:09:20 +0545

    Starting Project

---
+++ LinkStart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LinkStart.Startup))]
+namespace LinkStart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
