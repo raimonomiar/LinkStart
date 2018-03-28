f90ad6a34e79527696958aeef44d0110405f0458
[raimonomiar] <[raimonomiar@gmail.com]>
2018-03-28 17:55:53 +0545

    Finished authorization work

--- LinkStart/Controllers/AccountController.cs
+++ LinkStart/Controllers/AccountController.cs
@@ -76,7 +76,7 @@ namespace LinkStart.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -149,15 +149,18 @@ namespace LinkStart.Controllers
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Register(RegisterViewModel model)
-        {
+            {
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = model.Username, Email = model.Email,FirstName = model.FirstName,LastName=model.LastName};
+                var user = new User { UserName = model.Username, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
+                    await UserManager.AddToRoleAsync(user.Id, "User");
+
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
